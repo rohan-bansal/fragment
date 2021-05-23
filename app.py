fdfb6b7eb70530603ccdbcae2dcac8a553e61cce
Rohan Bansal <bansalrohan18@gmail.com>
2021-05-22 19:23:38 -0700

    add base files

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import logging
+from logging import Formatter, FileHandler
+import os
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route('/')
+def home():
+    return render_template('pages/placeholder.home.html')
+
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+# Default port:
+if __name__ == '__main__':
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Secret key for session management
+SECRET_KEY = 'GbNYEqLb5IimxcfsRFNvb6MTC30Wmd1J'
